e3e598a0fc0d2c55288e8f9c672e89c3079dc44f
magazzinsw <skylar.wong@magazzin.com>
2022-03-15 14:08:37 +0800

    trying react hook form

--- crud/src/components/Form/BlankForm.js
+++ crud/src/components/Form/BlankForm.js
@@ -12,25 +12,38 @@ function BlankForm() {
 
     const questions = useSelector(state => state.staff.questionsBank)
 
-    let choices = () => {
+    let selection = () => {
         let selection = []
         for (let i = 1; i < 11; i++) {
-            selection.push(`<option value="${i}">${i}</option>`)
+            selection.push(i)
+            // selection.push(`<option value="${i}">${i}</option>`)
         }
         console.log(selection)
         return selection
     }
     // choices()
     let dropDown = () => {
+        let choices = []
+        for (let i = 1; i < 11; i++) {
+            choices.push(i)
+            // selection.push(`<option value="${i}">${i}</option>`)
+        }
 
         return (
-            <select name="choice" id="choice" type="text">
-                <option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option>
+            <select name="choice" id="choice" type="text" {...register("Rating", { required: true })}>
+                {choices.map((choice, index) =>
+                    (<option value={choice} key={index}>{choice}</option>)
+                )}
+                {/* <option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option> */}
             </select>
         )
     }
     // console.log(dropDown())
+    const disableInput = () => {
 
+    }
+    const onSubmit = data => console.log(data);
+    console.log(errors);
     const getQuestionContent = async () => {
 
         let response = await fetch("http://localhost:8080/review/questions", {
@@ -51,29 +64,78 @@ function BlankForm() {
     }, [])
 
     return (
+        <>
+            
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th width="60%"></th>
+                            <th width="10%">Rating 評分</th>
+                            <th width="30%">Comments 評語<br />(Required if scored 7 to 10  必填，若評分為7至10)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-        <form>
-            <table border="1">
-                <thead>
-                    <tr>
-                        <th width="60%"></th>
-                        <th width="10%">Rating 評分</th>
-                        <th width="30%">Comments 評語<br />(Required if scored 7 to 10 / 必填，若評分為7至10)</th>
-                    </tr>
-                </thead>
-                <tbody>
+                        {questions[0]?.show_header === "Y" &&
+                            <tr><td colSpan={3}><b>{questions[0]?.section_header}</b></td></tr>}
 
-                    {questions[0]?.show_header === "Y" && <tr><td colSpan={3}><b>{questions[0]?.section_header}</b></td></tr>}
+                        <tr>
+                            <td>{questions[0]?.question_text}</td>
+                            <td>{dropDown()}</td>
+                            <td>
+                                <textarea id='comments' name='comments' className='comments' rows={5} cols={30} placeholder="Comments 評語
+                        (Required if scored 7 to 10  必填，若評分為7至10)"></textarea>
+                            </td>
+                        </tr>
+                <input type="submit" />
+                                              
+                    {questions.map((question,index)=>(
+                        <>
+                        {question?.show_header === "Y" && 
+                        <tr key={index}><td colSpan={3}>{question?.section === 10 && <input type={'checkbox'}/>}<b>{question?.section_header}</b></td></tr>}
 
-                    <tr>
-                        <td>{questions[0]?.question_text}</td>
-                        <td>{dropDown()}</td>
-                        <td><input type="text" /></td>
-                    </tr>
+                        <tr>
+                            <td>{question?.question_text}<br/>{question?.question_subtext}</td>
+                            <td>{dropDown()}</td>
+                            <td>
+                                <textarea id='comments' name='comments' className='comments' rows={5} cols={30}></textarea>
+                            </td>
+                        </tr>
+                        </>
+                    ))
+                    }
+                   
 
-                </tbody>
-            </table>
-        </form>
+                    </tbody>
+                </table>
+                {/* <form onSubmit={handleSubmit(onSubmit)}>
+                {questions[0]?.show_header === "Y" &&
+                            <label>{questions[0]?.section_header}</label>}
+                <label>{questions[0]?.question_text}</label>
+                <label>{questions[0]?.question_subtext}</label>
+                <select name="choice" {...register("Rating", { required: true })}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
+                <textarea {...register("Comment", {})} placeholder="Comments 評語
+                        (Required if scored 7 to 10  必填，若評分為7至10)"/>
+                <input type="checkbox" placeholder="supervisor" {...register("supervisor", { maxLength: 12 })} />
+
+                <input type="submit" />
+            </form> */}
+
+            </form>
+        </>
+
     )
 }
 
