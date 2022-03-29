6300fcfe907b3a812b76b0755bb2e8a6cb55be15
magazzinsw <skylar.wong@magazzin.com>
2022-03-29 16:10:58 +0800

    get score method from get to post request

--- api/route/route.js
+++ api/route/route.js
@@ -22,7 +22,7 @@ export function route() {
     // router.get('/review/ans+score_insert',)
     router.put('/review/ans_update',control.updateAnswer)
     router.post('/review/ans_insert',control.insertAnswer)
-    router.get('/review/score_get',control.getScores)
+    router.post('/review/score_get',control.getScores)
     router.put('/review/score_update',control.updateScore)
     router.post('/review/score_insert',control.insertScore)
     router.get('/review/score_summary_get',control.getScoreSummary)
