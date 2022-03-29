fcc1dd05432e89d2f2a32d5e8e2f0a0cd70969b6
magazzinsw <skylar.wong@magazzin.com>
2022-03-29 17:19:49 +0800

    refactor form to table

--- crud/src/components/Form/SubmittedForm.js
+++ crud/src/components/Form/SubmittedForm.js
@@ -65,18 +65,19 @@ function SubmittedForm({ assign_type }) {
             <h1>Performance Appraisal Form 表現評估表</h1>
             {/* {console.log(score)} */}
             <div width="100%" id="form-container" className='submitted-form'>
-                <table>
+                <table border="1">
                     <tbody>
+                        <tr>
+                            <td></td>
+                            <td>Rating</td>
+                            <td>Comments 評語<br />(Required if scored 7 to 10  必填，若評分為7至10)</td>
+                        </tr>
                         {answers.filter(answer => answer.section !== 20).map((answer, index) => (
                             <>
-                                <tr>
-                                    <td></td>
-                                    <td>Rating</td>
-                                    <td>Comments 評語<br />(Required if scored 7 to 10  必填，若評分為7至10)</td>
-                                </tr>
                                 {answer?.show_header === "Y" &&
-                                    <tr style={{ display: "flex", justifyCotent: "space-between", alignItems: "left", borderBottom: "solid" }}>
-                                        <td></td><b>{answer?.section_header}</b>
+                                    <tr >
+                                        <td colSpan={3}><b>{answer?.section_header}</b></td>
+
                                     </tr>}
                                 <tr>
                                     <td>
@@ -100,78 +101,46 @@ function SubmittedForm({ assign_type }) {
 
                     </tbody>
                 </table>
+                <table border="1">
+                    <tbody className='answer'>
+                        {answers.filter(answer => answer.section == 20).map((answer, index) => (
+                            <>
+                                <tr>
+                                    <td>
+
+                                        {answer?.show_header === "Y" &&
+                                            <label key={index}> <b>{answer?.section_header}</b></label>}
+                                        <label name="t_id" value={answer?.t_id} type="hidden" />
+
+                                        <label name="section" value={answer?.section} type="hidden" />
+                                        <label name="question_id" value={answer?.question_id} type="hidden" />
+                                        <label name="form_id" value={answer?.form_id} type="hidden" />
+
+                                        <b>{answer?.question_text}<br />{answer?.question_subtext}</b>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className='comments' id={`S${answer?.section.toString()}Q${answer?.question_id.toString()}_cmt`}>
+                                        {answer?.comments}
+
+                                        <input name="choice_id" className={`choice section_${answer?.section}`} id={`S${answer?.section.toString()}Q${answer?.question_id.toString()}_choice`} type="hidden" style={{ display: "inline-block" }} disabled />
+                                    </td>
+                                </tr>
+                            </>
+                        ))}
+                    </tbody>
+
+
+                </table>
 
 
 
-                {answers.filter(answer => answer.section !== 20).map((answer, index) => (
-                    <table style={{ borderTop: "solid" }}>
-                        <tbody>
-                            {answer?.show_header === "Y" &&
-                                <tr style={{ display: "flex", justifyCotent: "space-between", alignItems: "left", borderBottom: "solid" }}>
-                                    <td></td><b>{answer?.section_header}</b>
-                                </tr>}
-
-                            <div className='qna-container' style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                                <div style={{ textAlign: "left" }}>
-                                    <label>{answer?.question_text}</label>
-                                    <br />
-                                    <label>{answer?.question_subtext}</label>
-                                </div>
-                                {<form className='answer' width="40%" style={{ borderLeft: "dashed 1px", paddingLeft: "5px" }}>
-                                    <label name="t_id" value={answer?.t_id} type="hidden" />
-
-                                    <label name="section" value={answer?.section} type="hidden" />
-                                    <label name="question_id" value={answer?.question_id} type="hidden" />
-                                    <label name="form_id" value={answer?.form_id} type="hidden" />
-                                    <label style={{ display: "inline-block", marginRight: "10px" }}>Rating 評分</label>
-                                    {/* {dropDown(answer?.section, answer?.question_id)} */}
-                                    <select
-                                        name="choice_id"
-                                        className={`choice_id section_${answer?.section}`}
-                                        id={`S${answer?.section.toString()}Q${answer?.question_id.toString()}_choice`}
-                                        type="text"
-                                        style={{ display: "inline-block" }}
-                                        defaultValue={answer?.choice_id}>
-                                        <td>{answer?.choice_id}</td>
-                                        {/* <option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option> */}
-                                    </select>
-
-                                    <textarea
-                                        key={index}
-                                        name='comment'
-                                        className={`comments section_${answer?.section}`}
-                                        align="right" type="text"
-                                        id={`S${answer?.section.toString()}Q${answer?.question_id.toString()}_cmt`}
-                                        rows={5}
-                                        cols={30} placeholder="Comments 評語
-                                        (Required if scored 7 to 10  必填，若評分為7至10)"
-                                        // value={}
-                                        defaultValue={answer?.comments}
-                                        onChange={e => {
-                                            // console.log(e.target)
-                                            console.log(e.target.value)
-                                            // console.log(e.target.key)
-                                        }}
-                                    ></textarea>
-
-                                    {answer?.sales_figure === "Y" && <table style={{ border: "solid" }}>
-                                        <tr><td style={{ width: "max-content" }}></td><td>Employee</td><td>Store Avg.</td></tr>
-                                        <tr><td style={{ width: "max-content" }}>Avg. Monthly Sales</td><td><label style={{ width: "80px" }} name='emp_mon_sales' defaultValue={answer?.emp_mon_sales} /></td><td><label style={{ width: "80px" }} name='store_mon_sales' defaultValue={answer?.store_mon_sales} /></td></tr>
-                                        <tr><td style={{ width: "max-content" }}>Avg. Sales/Transaction</td><td><label style={{ width: "80px" }} name='emp_avg_sales' defaultValue={answer?.emp_avg_sales} /></td><td><label style={{ width: "80px" }} name='store_avg_sales' defaultValue={answer?.store_avg_sales} /></td></tr>
-
-                                    </table>}
-
-                                </form>
-                                }
-                            </div>
-                        </tbody>
-                    </table>
-                ))}
+               
 
             </div>
-            <footer style={{ display: "flex", justifyCotent: "space-evenly", position: "fixed", bottom: "0px" }}>
+            <footer >
                 <button onClick={() => window.print()}>Print</button>
-                <button><a href='#form-container'>Back to Top</a></button>
+                <button><a href='#top'>Back to Top</a></button>
                 <button>Average: <span className='avg'>{assign_type === "S" ? score?.score_avg_S : score?.score_avg_T}</span> Total: <span className='ttl'>{assign_type === "S" ? score?.score_ttl_S : score?.score_ttl_T}</span></button>
             </footer>
         </>
