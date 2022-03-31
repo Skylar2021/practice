f117f0ffebcea183c2f15e1e432e708170018408
magazzinsw <skylar.wong@magazzin.com>
2022-03-31 15:51:33 +0800

    apply variable in jsx instead of function

--- crud/src/components/Summary.js
+++ crud/src/components/Summary.js
@@ -1,26 +1,30 @@
 import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 
 import cookies from 'react-cookies';
 
 import { selfReview, tdReview } from '../app/slice.js'
 // import BlankForm from "./Form/BlankForm.js";
 
-export default function Summary() {
+export default function Summary({self_status, location}) {
     const dispatch = useDispatch()
+    const {state} = useLocation
+    console.log(state)
 
     const { self_review, top_down_review } = useSelector(state => state.staff)
+    const [self_review_status, setSelf_review_status] = useState(self_review.status)
+    // const [Td_review_status,setTd_review_status] = useState("") 
 
     const selfReviewData = async () => {
-        console.log('selfReviewData fired')
+        // console.log('selfReviewData fired')
         let res = await fetch("http://localhost:8080/review/get_self_review_summary", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ "id": `${cookies.load('userData')?.staff_id}` }),
             credentials: 'include'
         })
-        if (res.ok ) {
+        if (res.ok) {
             let result = await res.json()
             console.log(result)
             dispatch(selfReview(result))
@@ -28,7 +32,7 @@ export default function Summary() {
         } else {
             let result = await res.json()
             console.log(result)
-            
+
         }
     }
 
@@ -51,13 +55,13 @@ export default function Summary() {
 
     const selfReviewStatus = (status) => {
         if (status === 1) {
-            return "Draft"
+            setSelf_review_status("Draft")
         } else if (status === 2) {
-            return "Submitted"
+            setSelf_review_status("Submitted")
         } else if (status === null) {
-            return "no self review"
+            setSelf_review_status("no self review")
         } else {
-            return status
+            setSelf_review_status(status)
         }
     }
     const tdReviewStatus = (status) => {
@@ -73,12 +77,27 @@ export default function Summary() {
     }
     useEffect(() => {
         selfReviewData()
-        // console.log("cookies self_review: ")
-        // console.log(cookies.load("self_review"))
+        // dispatch(selfReviewData(cookies.load('userData')?.staff_id))
         topDownReviewData()
-        // console.log("cookies top_down_review: ")
-        // console.log(cookies.load("top_down_review"))
+        selfReviewStatus(self_review.status)
     }, [])
+    // useEffect(() => {
+    //     selfReviewData()        
+    //     topDownReviewData()
+    //     selfReviewStatus(self_review.status)        
+    // }, [])
+    useEffect(() => {
+        console.log(self_review.status)
+
+        selfReviewStatus(self_review.status)
+    }, [self_review.status])
+
+    // console.log("cookies: self_review")
+    // console.log(cookies.load("self_review"))
+    // console.log("store: self_review")
+    // console.log(self_review)
+    // console.log("redner")
+
 
     return (
         <>
@@ -123,7 +142,10 @@ export default function Summary() {
                     <tr>
                         <td>{self_review?.close_date?.slice(0, (self_review.close_date.indexOf('T')))}</td>
                         <td>{self_review?.completion_dt?.slice(0, (self_review.date_joined.indexOf('T')))}</td>
-                        <td>{selfReviewStatus(self_review?.status)}</td>
+                        {/* <td>{cookies.load("self_review")?.status === 1 ?"Draft": cookies.load("self_review")?.status === 2 ? "Submitted" : "no self review" }</td> */}
+                        <td>{self_review_status}</td>
+                        {/* <td>{self_status ? self_review_status : this.location.state}</td> */}
+
                         <td><Link to='/self_review'>{self_review?.t_id ? self_review.t_id : "click to create"}</Link></td>
                     </tr>
                 </thead>
