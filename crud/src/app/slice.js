e71b812344d4be22d2d8df8d17f5ae20ec171e2b
magazzinsw <skylar.wong@magazzin.com>
2022-03-31 15:52:29 +0800

    createAsyncThunk testing

--- crud/src/app/slice.js
+++ crud/src/app/slice.js
@@ -7,27 +7,63 @@ const initialState = {
     supervisor: false,
     questionsBank: [],
     answers: [],
-    score:{},
-    
+    score: {},
+    user: {}
+
     // currentUser:"",
     // pwd: "",
     // id: ""
 }
 
-// export const testing = createAsyncThunk(
-//     'staff info', 
-//     async () =>{
-//         let res = await fetch("http://localhost:8080/")
-//         if(res.ok){
-//             return res.json()
-//         }
-//     }
+export const testing = createAsyncThunk(
+    'staff info',
+    async () => {
+        try {
 
-// )
+            let res = await fetch("http://localhost:8080/login",
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ "id": `1-2878`, "password": `0000` }),
+                    credentials: 'include'
+                })
+            let result = res.json()
+            console.log(result)
+            return result
+
+        } catch (error) {
+            return error
+
+        }
+
+    }
+
+)
+
+export const selfReviewData = createAsyncThunk(
+    'self reivew detail',
+    async (staff_id) => {
+        try {
+            let res = await fetch("http://localhost:8080/review/get_self_review_summary", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ "id": `${staff_id}` }),
+                credentials: 'include'
+            })
+            if (res.ok) {
+                let result = await res.json()
+                console.log(result)
+                return result
+            }
+        } catch (error) {
+            return error
+        }
+    }
+)
 
 
 export const staffInfo = createSlice({
-    name: 'Log in and out',
+    name: 'Appraisal',
     initialState,
     reducers: { //non-async reducers
 
@@ -59,15 +95,18 @@ export const staffInfo = createSlice({
         setSelfReviewStatus: (state, action) => {
             state.self_review.status = action.payload
         }
-       
+
     },
     extraReducers: { // async reducers 
-        // [testing.fulfilled]: (state, action) => {
-        //     return [...action.payload];
-        //   }
+        [testing.fulfilled]: (state, { payload }) => {
+            state.user = payload
+        },
+        [selfReviewData.fulfilled]: (state, { payload }) => {
+            state.self_review = payload
+        }
     }
 })
 
-export const { login, logout, selfReview, tdReview, getQuestions, toggleSupervisor, getAnswers,getScore,setSelfReviewStatus } = staffInfo.actions
+export const { login, logout, selfReview, tdReview, getQuestions, toggleSupervisor, getAnswers, getScore, setSelfReviewStatus } = staffInfo.actions
 export default staffInfo.reducer
 
